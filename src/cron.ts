58dcf65bcfd93d0e43f5bdf9e52ffdf7f0fbfe97
Kingsley Victor <javaprodigy56@gmail.com>
2022-04-19 21:33:34 +0100

    add networks.json

--- src/cron.ts
+++ src/cron.ts
@@ -4,8 +4,52 @@ import logger from './logger';
 import { assetsUrl, coinAPIRoot, coinAPIKey } from './env';
 import { _apiRequest } from './utils';
 import * as CONSTANTS from './constants';
+import networks from './networks.json';
 
 export default class CronService {
+  static _fetchAssets() {
+    cron
+      .schedule('* */1 * * * *', async () => {
+        try {
+          let coinsList: Array<any> = [];
+          const blockchainsResponse = await _apiRequest(
+            `${assetsUrl}/assets/list`,
+            { method: 'GET', headers: { Accepts: 'application/json' } },
+            'json'
+          );
+
+          for (const blockchain of blockchainsResponse.result) {
+            const blockchainInfo = await _apiRequest(
+              `${assetsUrl}/assets/${blockchain}/info`,
+              { method: 'GET', headers: { Accepts: 'application/json' } },
+              'json'
+            );
+            coinsList = [...coinsList, blockchainInfo.result];
+
+            if (networks.value.includes(blockchain)) {
+              const assetsAddressesResponse = await _apiRequest(
+                `${assetsUrl}/assets/tokens/${blockchain}/addresses`,
+                { method: 'GET', headers: { Accepts: 'application/json' } },
+                'json'
+              );
+
+              for (const address of assetsAddressesResponse.result) {
+                const assetInfo = await _apiRequest(
+                  `${assetsUrl}/assets/tokens/${blockchain}/${address}/info`,
+                  { method: 'GET', headers: { Accepts: 'application/json' } },
+                  'json'
+                );
+                coinsList = [...coinsList, assetInfo.result];
+              }
+            }
+          }
+        } catch (error: any) {
+          logger(error.message);
+        }
+      })
+      .start();
+  }
+
   static _fetchCoinPrices() {
     cron
       .schedule('*/30 * * * * *', async () => {
@@ -39,6 +83,7 @@ export default class CronService {
             record = Object.fromEntries(record);
 
             const _result = await redis.simpleSet(CONSTANTS.REDIS_PRICES_KEY, JSON.stringify(record));
+
             logger('Redis response: %s', _result);
           }
         } catch (error: any) {
