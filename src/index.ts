7fef35a6cbd889095f177181e0fddedd6ef31353
Kingsley Victor <javaprodigy56@gmail.com>
2022-04-20 21:33:51 +0100

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import logger from './logger';
 import { port as PORT } from './env';
+import { _fetchAssetList, _fetchCoinPrices } from './functions';
 
 const port: number = parseInt(PORT || '15500');
 const app: express.Express = express();
 
-app.listen(port, () => logger('Server running on port %d', port));
+app.listen(port, () => {
+  logger('Server running on port %d', port);
+  (async () => {
+    await _fetchAssetList();
+    await _fetchCoinPrices();
+  })();
+});
